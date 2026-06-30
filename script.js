2407327c07e93ad2c39723484a2ef030ebed340f
krishnachauhan0204-collab <krishna.chauhan.0204@gmail.com>
2026-06-30 22:08:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function welcome() {
     let name = document.getElementById("name").value;
     document.getElementById("result").innerHTML =
-        "Welcome " + name + "! Your career journey starts here.";
+    "Welcome " + name + "!";
+}
+
+function career() {
+    let interest = document.getElementById("interest").value;
+
+    if (interest == "Engineering") {
+        document.getElementById("result").innerHTML = "Best Career: Software Engineer";
+    }
+    else if (interest == "Medical") {
+        document.getElementById("result").innerHTML = "Best Career: Doctor";
+    }
+    else if (interest == "Commerce") {
+        document.getElementById("result").innerHTML = "Best Career: Chartered Accountant";
+    }
+    else if (interest == "Arts") {
+        document.getElementById("result").innerHTML = "Best Career: Graphic Designer";
+    }
+    else if (interest == "Cyber Security") {
+        document.getElementById("result").innerHTML = "Best Career: Cyber Security Analyst";
+    }
 }
